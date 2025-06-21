84a9a5c761180a3bbd34709c34c7e938a43e2073
ArytechRocher <yetotodeabdelr@gmail.com>
2025-06-21 20:22:00 +0100

    Test daffichage de base pour débogage

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,25 @@
-import 'package:flutter/material.dart';
-import 'package:audio_service/audio_service.dart';
-import 'background_audio.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final audioHandler = await AudioService.init(
-    builder: () => RadioAudioHandler(),
-    config: const AudioServiceConfig(
-      androidNotificationChannelId: 'com.alrc.radio.channel.audio',
-      androidNotificationChannelName: 'ALRC Radio Business',
-      androidNotificationOngoing: true,
-    ),
-  );
-  runApp(ALRCRadioApp(audioHandler: audioHandler));
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
 }
 
-class ALRCRadioApp extends StatelessWidget {
-  final AudioHandler audioHandler;
-  const ALRCRadioApp({super.key, required this.audioHandler});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ALRC Radio Business',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text("ALRC Radio Business")),
+        appBar: AppBar(
+          title: Text('ALRC Radio Business'),
+          backgroundColor: Colors.redAccent,
+        ),
         body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              audioHandler.play();
-            },
-            child: const Text("Écouter en direct"),
+          child: Text(
+            'Bienvenue sur la Radio Business !',
+            style: TextStyle(fontSize: 20),
           ),
         ),
       ),
