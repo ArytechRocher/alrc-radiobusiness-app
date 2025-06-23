c6aafdc1d08e70eda3fb68d42876c2ae1985618c
ArytechRocher <yetotodeabdelr@gmail.com>
2025-06-23 09:33:34 +0100

    redeployement

--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,31 @@ import 'ui/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final audioHandler = await initAudioService();
-  runApp(MyApp(audioHandler));
+
+  final audioHandler = await AudioService.init(
+    builder: () => MyAudioHandler(),
+    config: AudioServiceConfig(
+      androidNotificationChannelId: 'com.example.alrc_radio.channel.audio',
+      androidNotificationChannelName: 'ALRC Radio Audio',
+      androidNotificationOngoing: true,
+    ),
+  );
+
+  runApp(MyApp(audioHandler: audioHandler));
 }
 
 class MyApp extends StatelessWidget {
   final AudioHandler audioHandler;
-  const MyApp(this.audioHandler, {super.key});
+  const MyApp({required this.audioHandler, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AudioServiceHandlerProvider(
-      handler: audioHandler,
-      child: MaterialApp(
-        title: 'ALRC Radio Business',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          appBarTheme: const AppBarTheme(centerTitle: true),
-        ),
-        home: const HomeScreen(),
+    return MaterialApp(
+      title: 'ALRC Radio Business',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
       ),
+      home: HomeScreen(audioHandler: audioHandler),
     );
   }
 }
-
